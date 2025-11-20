80d43b860e8740ae2dd98f96af734c8d3013ef81
Hugo Pozzi <hugopozzi807@gmail.com>
2025-11-20 19:49:40 +0100

    Add item module and adapt code to incorporate egg item spawning

--- make.bat
+++ make.bat
@@ -2,4 +2,5 @@ c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o ./build/poule.o ./proj
 c:\gbdk\bin\lcc -c -o ./build/audio.o ./project/src/audio.c
 c:\gbdk\bin\lcc -c -o ./build/player.o ./project/src/player.c
 c:\gbdk\bin\lcc -c -o ./build/hud.o ./project/src/hud.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o ./build/poule.gb ./build/poule.o ./build/player.o ./build/audio.o ./build/hud.o
+c:\gbdk\bin\lcc -c -o ./build/item.o ./project/src/item.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o ./build/poule.gb ./build/poule.o ./build/player.o ./build/audio.o ./build/hud.o ./build/item.o
