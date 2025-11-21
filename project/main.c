fb2e2bb240e5982a3cbdc2dcca5bacbcb792a040
Hugo Pozzi <hugopozzi807@gmail.com>
2025-11-21 01:08:06 +0100

    Add random egg spawn logic

--- project/main.c
+++ project/main.c
@@ -15,8 +15,17 @@
 #include "./assets/ChickenLittle.c"
 #include "./assets/EggLittle.c"
 
+#define SCREEN_MIN_X 24
+#define SCREEN_MAX_X 152
+#define SCREEN_MIN_Y 56
+#define SCREEN_MAX_Y 128
+
 Player player;
 ItemManager item_manager;
+
+void new_rand_egg( UBYTE x, UBYTE y) {
+    item_spawn(&item_manager, x, y, ITEM_EGG, 1);
+}
 
 void on_item_collected(ItemType type, uint8_t value, uint8_t item_index) {
     switch(type) {
@@ -24,6 +33,18 @@ void on_item_collected(ItemType type, uint8_t value, uint8_t item_index) {
             player_add_score(&player, value);
             /* audio_play_sfx(...) */
             item_remove(&item_manager, item_index);
+
+            UBYTE x, y;
+            UWORD seed;
+            puts("");
+            seed = DIV_REG;
+            initarand(seed);
+            x = rand();
+            y = arand();
+            x = x % (SCREEN_MAX_X - SCREEN_MIN_X);
+            y = y % (SCREEN_MAX_Y - SCREEN_MIN_Y);
+
+            new_rand_egg(x + SCREEN_MIN_X, y + SCREEN_MIN_Y);
             break;
         default:
             break;
@@ -38,21 +59,32 @@ void init_game(void) {
     set_bkg_data(38, 11, backTile);
     set_bkg_submap(0, 0, 20, 18, backMap, 20);
     
+    UBYTE x, y;
+    UWORD seed;
+    puts("");
+    waitpad(0xFF);
+    waitpadup();
+    seed = DIV_REG;
+    initarand(seed);
+    x = rand();
+    y = arand();
+
+    x = x % (SCREEN_MAX_X - SCREEN_MIN_X);
+    y = y % (SCREEN_MAX_Y - SCREEN_MIN_Y);
+
+    hud_default();
+    
     set_sprite_data(0, 4, chickenLittleSprite);
     player_init(&player, 88, 80);
     
     set_sprite_data(4, 4, EggLittleSprite);
     item_manager_init(&item_manager);
-    item_spawn(&item_manager, 40, 64, ITEM_EGG, 1);
+    new_rand_egg(x + SCREEN_MIN_X, y + SCREEN_MIN_Y);
 
     SHOW_BKG;
     SHOW_SPRITES;  
     SHOW_WIN;  
     DISPLAY_ON;
-}
-
-void new_rand_egg(void) {
-    // TODO
 }
 
 void update_game(void) {
